f7a1f664cde8b859c4ac4c6170dc27db2c02f733
DamianTrzonek <damian.trzonek@gmail.com>
2018-03-14 20:44:06 +0100

    First Commit modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
+    public static void main (String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-public static void main (String[] args){
-    System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy.");
+
+        Calculator calc = new Calculator ();
+
+        int addResult = calc.add(10,5);
+
+        if(addResult == 15) {
+            System.out.println("Add test OK!");
+        } else {
+            System.out.println("Add test FAIL!");
+        }
+
+        int substractResult = calc.substract(10,5);
+
+        if(substractResult == 5){
+            System.out.println("Substract test OK!");
+        } else {
+            System.out.println("Substract test FAIL!");
+        }
 }
 }
