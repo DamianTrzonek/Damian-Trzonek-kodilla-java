e7ca1a8c99f14e24f971ec1538ca5ed7b3ace5ea
DamianTrzonek <damian.trzonek@gmail.com>
2018-03-21 20:23:55 +0100

    Zalegle i poprawki

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,14 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.SaySomething;
+
+public class StreamMain {
+
+    public static void main(String[] args){
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example of text."));
+    }
+}
