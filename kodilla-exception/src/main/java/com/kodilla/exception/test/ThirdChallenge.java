eaa4d915c89c2cbf154f435e664d6ab01d637364
DamianTrzonek <damian.trzonek@gmail.com>
2018-04-04 19:09:28 +0200

    Second Commit modul 6.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ThirdChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ThirdChallenge.java
@@ -1,18 +1,14 @@
 package com.kodilla.exception.test;
 
 public class ThirdChallenge {
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
         FlightFinder flightFinder = new FlightFinder();
-       try {
-           flightFinder.findFlight(new Flight("Warsaw", "Berlin"));
-       }catch (RouteNotFoundException e){
-           System.out.println("Error! Route not found!");
-       }
-       try {
-           flightFinder.findFlight(new Flight());
-       }catch (RouteNotFoundException e){
-           System.out.println("Error! Route not found!");
-       }
+        try {
+            flightFinder.findFlight(new Flight());
+            flightFinder.findFlight(new Flight("Warsaw", "Berlin"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Error! Route not found!");
+        }
     }
 }
