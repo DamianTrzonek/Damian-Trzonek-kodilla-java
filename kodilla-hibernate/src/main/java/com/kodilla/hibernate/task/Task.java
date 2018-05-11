1e61480eb88a7c35227c9108a87cca5749337dd0
DamianTrzonek <damian.trzonek@gmail.com>
2018-05-12 00:29:22 +0200

    First Commit module 13.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -5,6 +5,27 @@ import com.kodilla.hibernate.tasklist.TaskList;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+@NamedQueries({
+        @NamedQuery(
+                name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration > 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
+        )
+})
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS " +
+                "WHERE DATEDIFF(DATE_ADD(CREATED, INTERVAL DURATION DAY), NOW()) > 5",
+        resultClass = Task.class
+)
+
 @Entity
 @Table(name = "TASKS")
 public class Task {
@@ -70,7 +91,7 @@ public class Task {
         this.taskFinancialDetails = taskFinancialDetails;
     }
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "TASKLIST_ID")
     public TaskList getTaskList() {
         return taskList;
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany() {
@@ -60,4 +64,91 @@ public class CompanyDaoTestSuite {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testNamedQueries() {
+        //Given
+        companyDao.deleteAll();
+        employeeDao.deleteAll();
+
+        Employee johnSmith = new Employee("John","Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+        Employee andySmith = new Employee("Andy", "Smith");
+        Employee camillaKovalsky = new Employee("Camilla","Kovalsky");
+        Employee joshuaSmith = new Employee("Joshua", "Smith");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMasters = new Company("Data Masters");
+        Company greyMatter = new Company("Grey Matter");
+        Company greenLight = new Company("Green Light");
+        Company datCode = new Company("Dat Code");
+        Company codeVaders = new Company("Code Vaders");
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMasters);
+        int dataMastersId = dataMasters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+        companyDao.save(greenLight);
+        int greenLightId = greenLight.getId();
+        companyDao.save(datCode);
+        int datCodeId = datCode.getId();
+        companyDao.save(codeVaders);
+        int codeVadersId = codeVaders.getId();
+
+        employeeDao.save(johnSmith);
+        int johnSmithId = johnSmith.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieClarcksonId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaKovalskyId = lindaKovalsky.getId();
+        employeeDao.save(andySmith);
+        int andySmithId = andySmith.getId();
+        employeeDao.save(camillaKovalsky);
+        int camillaKovalskyId = camillaKovalsky.getId();
+        employeeDao.save(joshuaSmith);
+        int joshuaSmithId = johnSmith.getId();
+
+
+        //When
+       List<Company> companiesStaringWithLettersDat = companyDao.retrieveCompaniesWhereName("Dat");
+       List<Company> companiesStartingWithLettersSof = companyDao.retrieveCompaniesWhereName("Sof");
+       List<Company> companiesStartingWithLettersGre = companyDao.retrieveCompaniesWhereName("Gre");
+       List<Company> companiesStartingWithLettersCod = companyDao.retrieveCompaniesWhereName("Cod");
+       List<Employee> listOfSmith =  employeeDao.retrieveEmployeesWithLastname("Smith");
+       List<Employee> listOfKovalsky = employeeDao.retrieveEmployeesWithLastname("Kovalsky");
+       List<Employee> listofClarckson = employeeDao.retrieveEmployeesWithLastname("Clarckson");
+
+        //Then
+       try{
+           Assert.assertEquals(2, companiesStaringWithLettersDat.size());
+        Assert.assertEquals(1, companiesStartingWithLettersSof.size());
+        Assert.assertEquals(2, companiesStartingWithLettersGre.size());
+        Assert.assertEquals(1, companiesStartingWithLettersCod.size());
+        Assert.assertEquals(3, listOfSmith.size());
+        Assert.assertEquals(2, listOfKovalsky.size());
+        Assert.assertEquals(1, listofClarckson.size());
+    } catch (Exception e) {
+           e.printStackTrace();
+       }
+       //CleanUp
+        try {
+           companyDao.delete(softwareMachineId);
+           companyDao.delete(dataMastersId);
+           companyDao.delete(greyMatterId);
+           companyDao.delete(greenLightId);
+           companyDao.delete(datCodeId);
+           companyDao.delete(codeVadersId);
+           employeeDao.delete(johnSmithId);
+           employeeDao.delete(stephanieClarcksonId);
+           employeeDao.delete(lindaKovalskyId);
+           employeeDao.delete(andySmithId);
+           employeeDao.delete(camillaKovalskyId);
+           employeeDao.delete(joshuaSmithId);
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+       }
 }
