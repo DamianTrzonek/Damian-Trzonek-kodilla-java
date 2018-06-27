dad7c915142999232dd350b92278186421302fff
DamianTrzonek <damian.trzonek@gmail.com>
2018-06-27 17:00:27 +0200

    Added missing classes from last commit

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Book.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Book.java
@@ -1,4 +1,31 @@
 package com.kodilla.patterns2.adapter.bookclasifier.librarya;
 
 public class Book {
+    private final String author;
+    private final String title;
+    private final int publicatonYear;
+    private final String signature;
+
+    public Book(final String author, final String title, final int publicatonYear, final String signature) {
+        this.author = author;
+        this.title = title;
+        this.publicatonYear = publicatonYear;
+        this.signature = signature;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublicatonYear() {
+        return publicatonYear;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
 }
