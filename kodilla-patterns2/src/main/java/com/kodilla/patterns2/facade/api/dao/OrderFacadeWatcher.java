6d137262295f7fcf6e35b7875e09649ae173e8c6
DamianTrzonek <damian.trzonek@gmail.com>
2018-07-07 17:56:14 +0200

    AOP for processOrder() from OrderFacade

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/dao/OrderFacadeWatcher.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.facade.api.dao;
+
+import com.kodilla.patterns2.facade.api.OrderDto;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+    "&& target(object)")
+    public void logEvent(Object object) {
+        LOGGER.info("Order processing has started for Class " + object.getClass().getName());
+
+    }
+}
